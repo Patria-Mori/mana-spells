11fd9a8bb8146b3fb8d5e9fbf57bfd4300846010
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2023-04-22 15:20:04 +0200

    Settup automatic mana deduction.
    Closes #3.

--- scripts/moduleHooks.js
+++ scripts/moduleHooks.js
@@ -24,3 +24,34 @@ Hooks.on("ready", async function () {
     // SpellFlagApi.initialiseAllSpellFlags(); TODO: Uncomment this later (keeping it commented since porting all the spells might cause some errors).
     // SpellFlagApi.updateAllSpellFlags();
 });
+
+/**
+ * Fires when an item is used, after the measured template has been created if one is needed.
+ * @param {Item5e} item Item being used.
+ * @param {ItemUseConfiguration} config Configuration data for the roll.
+ * @param {ItemUseOptions} options Additional options for configuring item usage.
+ */
+Hooks.on("dnd5e.useItem", async function(item, config, options) {
+    callSpellApi(item);
+});
+
+/**
+ * Fires when an item is used, calling the SpellApi to cast the spell if it is a spell (and not a cantrip).
+ * @param {Item5e} item Item being used.
+ */
+async function callSpellApi(item) {
+    if (item.type == "spell" && item.system.level > 0) {
+        const actorId = item.parent._id;
+        const itemId = item._id;
+
+        const spellFlag = await SpellFlagApi.getItemSpellFlag(actorId, itemId);
+
+        const baseLvl = spellFlag.baseLvl;
+        const castLvl = item.system.level;
+        const reaction = item.system.activation.type.startsWith("reaction");
+
+        const spell = new Spell(baseLvl, castLvl, spellFlag.circles, reaction);
+        
+        SpellApi.castSpell(spell, actorId);
+    } 
+}
